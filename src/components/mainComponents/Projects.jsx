82573fe5c9bfe49cf5e0fc05f2c7a483b2ac6034
Maor Ovadya <maorovadya@icloud.com>
2024-02-23 08:09:26 -0800

    add projects01.00

--- src/components/mainComponents/Projects.jsx
+++ src/components/mainComponents/Projects.jsx
@@ -14,9 +14,40 @@ export default function Projects() {
       <section className="flex flex-col">
         {projects.map((project) => {
           return (
-            <div key={project.id}>
-              {project.image}
-              {project.project}
+            <div key={project.id} className="flex flex-col lg:flex-row ">
+              <div className="flex items-stretch w-full">{project.img}</div>
+              <section className=" text-[#222222] flex flex-col justify-center items-center tracking-wide w-full">
+                <div className="text-xl md:text-3xl text-center pb-10 pt-20 flex">
+                  {project.title}
+                  <button className=" text-white mx-10 flex items-center gap-5 text-lg md:text-2xl">
+                    <a
+                      className="bg-[#C60F52] p-2 rounded-md"
+                      target="_blank"
+                      href={project.gitRipo}
+                      rel="noreferrer"
+                    >
+                      <FaGithub />
+                    </a>
+                    <a
+                      className="bg-[#C60F52] p-2 rounded-md"
+                      target="_blank"
+                      href={project.webSite}
+                      rel="noreferrer"
+                    >
+                      <BsBoxArrowDownLeft />
+                    </a>
+                  </button>
+                </div>
+                <article className="mx-5 md:mx-0 md:w-[500px] p-5 rounded-md bg-[#C60F52] text-white leading-relaxed text-sm">
+                  {project.article}
+                </article>
+                <div className="text-center md:text-left md:w-[500px] my-10 leading-relaxed text-sm">
+                  <div className="text-xl underline underline-offset-2">
+                    Development tools:
+                  </div>
+                  <article>{project.developmentTools}</article>
+                </div>
+              </section>
             </div>
           );
         })}
@@ -24,97 +55,45 @@ export default function Projects() {
     </div>
   );
 }
+{
+  /* <div className="projects">
+<Title title="my work" number="02." />
+
+<section className="flex flex-col">
+  {projects.map((project) => {
+    return (
+      <div key={project.id}>
+        {project.image}
+        {project.project}
+      </div>
+    );
+  })}
+</section>
+</div> */
+}
 
 const projects = [
   {
-    id: 1,
-    project: (
-      <section className="flex flex-col md:flex-row ">
-        <div className="flex items-stretch w-full">
-          <img src={Timage} alt="" />
-        </div>
-        <section className=" text-[#222222] flex flex-col justify-center items-center tracking-wide w-full">
-          <div className="text-xl md:text-3xl text-center pb-10 pt-20 flex">
-            Teddies Vs Monster
-            <button className=" text-white mx-10 flex items-center gap-5 text-lg md:text-2xl">
-              <a
-                className="bg-[#C60F52] p-2 rounded-md"
-                target="_blank"
-                href="https://github.com/chicacode/teddies-vs-monsters-frontend"
-                rel="noreferrer"
-              >
-                <FaGithub />
-              </a>
-              <a
-                className="bg-[#C60F52] p-2 rounded-md"
-                target="_blank"
-                href="https://teddies-vs-monsters-frontend.vercel.app/"
-                rel="noreferrer"
-              >
-                <BsBoxArrowDownLeft />
-              </a>
-            </button>
-          </div>
-          <article className="md:w-[500px] p-5 rounded-md bg-[#C60F52] text-white leading-relaxed text-sm">
-            The E-Commerce Platform for the gaming board,Teddies Vs Monsters
-            Board Game, is an engaging and fun board game where players take on
-            the roles of teddy bears defending their realm against menacing
-            monsters.
-          </article>
-          <div className="text-center md:text-left md:w-[500px] my-10 leading-relaxed text-sm">
-            <div className="text-xl underline underline-offset-2">
-              Development tools:
-            </div>
-            <article>
-              Trello,Figma,TypeScript,Tailwind Css,NextJs,
-              Zuztand,Strapi,Stripe,Swiper
-            </article>
-          </div>
-        </section>
-      </section>
-    ),
+    id: 2,
+    title: "Teddies Vs Monster",
+    img: <img src={Timage} alt="" />,
+    article: `The E-Commerce Platform for the gaming board,Teddies Vs Monsters
+    Board Game, is an engaging and fun board game where players take on
+    the roles of teddy bears defending their realm against menacing
+    monsters.`,
+    developmentTools: `Trello,Figma,TypeScript,Tailwind Css,NextJs,
+    Zuztand,Strapi,Stripe,Swiper`,
+    gitRipo: "https://github.com/chicacode/teddies-vs-monsters-frontend",
+    webSite: "https://teddies-vs-monsters-frontend.vercel.app/",
   },
   {
-    id: 2,
-    project: (
-      <section className="flex flex-col md:flex-row ">
-        <div className="flex items-stretch w-full	">
-          <img src={Limage} alt="" />
-        </div>
-        <section className=" bg-[#222222] text-white flex flex-col justify-center items-center tracking-wide w-full">
-          <div className="text-xl md:text-3xl text-center py-20 flex">
-            Airline Website
-            <button className=" text-white mx-10 flex items-center gap-5 text-lg md:text-2xl">
-              <a
-                className="bg-[#C60F52] p-2"
-                target="_blank"
-                href="https://github.com/MaorOvadya/landingPage"
-                rel="noreferrer"
-              >
-                <FaGithub />
-              </a>
-              <a
-                className="bg-[#C60F52] p-2 rounded-md"
-                target="_blank"
-                href="https://main--lifeairline.netlify.app/"
-                rel="noreferrer"
-              >
-                <BsBoxArrowDownLeft />
-              </a>
-            </button>
-          </div>
-          <article className="md:w-[500px] p-5 rounded-md bg-[#C60F52] text-white leading-relaxed text-sm">
-            The Airline Website is a static, responsive website that exclusively
-            features the structural elements and CSS for its design.
-          </article>
-          <div className="text-center md:text-left md:w-[500px] my-10 leading-relaxed text-sm">
-            <div className="text-xl underline underline-offset-2">
-              Development tools:
-            </div>
-            <article>Html,Sass</article>
-          </div>
-        </section>
-      </section>
-    ),
+    id: 1,
+    title: "Airline Website",
+    img: <img src={Limage} alt="" />,
+    article: `The Airline Website is a static, responsive website that exclusively
+    features the structural elements and CSS for its design.`,
+    developmentTools: `Html,Sass`,
+    gitRipo: "https://github.com/MaorOvadya/landingPage",
+    webSite: "https://main--lifeairline.netlify.app/",
   },
 ];
