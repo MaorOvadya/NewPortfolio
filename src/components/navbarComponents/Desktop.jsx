5ba99fc07460520579ba0ff885d39fff96770473
Maor Ovadya <maorovadya@icloud.com>
2024-02-22 20:01:55 -0800

    fix nav-01.00

--- src/components/navbarComponents/Desktop.jsx
+++ src/components/navbarComponents/Desktop.jsx
@@ -6,31 +6,50 @@ export default function Desktop() {
     <div className="hidden md:block bg-[#222222] text-lg text-white fixed w-full">
       {({ active }) => (active ? "active" : "")}
       <ul className="flex justify-center  md:gap-5 lg:gap-10 py-5 ">
-        <li className="px-8 py-2 cursor-pointer">
-          <Link className="" to="home" spy={true} smooth="true" duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className="px-8 py-2 cursor-pointer">
-          <Link to="about" spy={true} smooth="true" duration={500}>
-            About
-          </Link>
-        </li>
-        <li className="px-8 py-2 cursor-pointer">
-          <Link to="projects" spy={true} smooth="true" duration={500}>
-            My Work
-          </Link>
-        </li>
-        <li className="px-8 py-2 cursor-pointer">
-          <Link to="contact" spy={true} smooth="true" duration={500}>
-            Contact
-          </Link>
-        </li>
-        <li className="px-8 py-2 cursor-pointer bg-[#C60F52] rounded-md hover:bg-[#9A0C40] transition-all duration-700 ease-in-out">
-          <a target="_blank" href={Resume + "#toolbar=0"} rel="noreferrer">
-            Resume
-          </a>
-        </li>
+        <Link
+          className="px-8 py-2  cursor-pointer"
+          to="home"
+          spy={true}
+          smooth="true"
+          duration={500}
+        >
+          Home
+        </Link>
+        <Link
+          className="px-8 py-2  cursor-pointer"
+          to="about"
+          spy={true}
+          smooth="true"
+          duration={500}
+        >
+          About
+        </Link>
+        <Link
+          className="px-8 py-2  cursor-pointer"
+          to="projects"
+          spy={true}
+          smooth="true"
+          duration={500}
+        >
+          My Work
+        </Link>
+        <Link
+          className="px-8 py-2  cursor-pointer"
+          to="contact"
+          spy={true}
+          smooth="true"
+          duration={500}
+        >
+          Contact
+        </Link>
+        <a
+          className="px-8 py-2 cursor-pointer bg-[#C60F52] rounded-md hover:bg-[#9A0C40] transition-all duration-700 ease-in-out"
+          target="_blank"
+          href={Resume + "#toolbar=0"}
+          rel="noreferrer"
+        >
+          Resume
+        </a>
       </ul>
     </div>
   );
